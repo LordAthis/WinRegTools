e52a2fb33c65a214ef45910ce70c80b7bede4e68
LordAthis <lordathis@gmail.com>
2026-04-21 01:55:01 +0200

    Add new options and fixes to Master.ps1 menu

--- Script/Master.ps1
+++ Script/Master.ps1
@@ -11,7 +11,14 @@ function Show-Menu {
     Write-Host "1. Hosszú nevek (LongPaths) kezelése"
     Write-Host "2. Lapozófájl ürítés (SwapDelete) kezelése"
     Write-Host "3. RPC Hiba javítása"
-    Write-Host "3. Kilépés"
+    Write-Host "4. WinSxS Hiba javítása-takarítása (Feleslegesen felhalmozott telepítő fájlok törlése"
+    Write-Host "5. Bring Keresések Letíltása"
+    Write-Host "6. Indexelés Letiíltása"
+    Write-Host "7. Bring StartMenüből kitíltásaa"
+    Write-Host "8. Telemertia Letíltása"
+    Write-Host "9. KB Checker - Windows Frissítések elemzése (Hiányzó fontos, és Felesleges, mert káros)"
+    Write-Host "10. Visszaállítási pont létrehozása"
+    Write-Host "X. Kilépés" -ForegroundColor Red
     Write-Host "----------------------------------------"
 }
 
@@ -22,6 +29,13 @@ do {
         "1" { & "$PSScriptRoot\LongPaths_On_Off.ps1" }
         "2" { & "$PSScriptRoot\SwapDeleteToShutdown.ps1" }
         "3" { & "$PSScriptRoot\RPCHelper_Fix.ps1" }
-        "4" { exit }
+        "4" { & "$PSScriptRoot\Clean-WinSxS.ps1" }
+        "5" { & "$PSScriptRoot\Disable-BingSearch.ps1" }
+        "6" { & "$PSScriptRoot\Disable-Indexing.ps1" }
+        "7" { & "$PSScriptRoot\Disable-StartMenuBring.ps1" }
+        "8" { & "$PSScriptRoot\Disable-Telemerty.ps1" }
+        "9" { & "$PSScriptRoot\KB_Checker.ps1" }
+        "10" { & "$PSScriptRoot\Create-RestorePoint.ps1" }
+        "x" { exit }
     }
 } while ($true)
