a3c6f9f267375f650047c04374b83f5aaeb4e4a6
LordAthis <lordathis@gmail.com>
2026-04-22 21:07:53 +0200

    Add script to disable Windows consumer features

---
+++ Scripts/Disable-ConsumerFeatures.ps1
@@ -0,0 +1,21 @@
+# --- Ébren tartás és Laptop figyelmeztetés ---
+Write-Host "![FIGYELEM] Hosszu folyamat kovetkezik! Hasznalj TOLTOT!" -ForegroundColor Yellow
+Write-Host "[*] Automatikus elalvas felfuggesztve..." -ForegroundColor Gray
+
+# API betöltése (dinamikus névvel, hogy ne legyen ütközés)
+$sig = '[DllImport("kernel32.dll")] public static extern uint SetThreadExecutionState(uint esFlags);'
+$type = Add-Type -MemberDefinition $sig -Name "Sleep$(Get-Random)" -Namespace "Win32" -PassThru
+
+# Futás alatt: Ébren tartás kényszerítése
+$type::SetThreadExecutionState([uint32]0x80000001) 
+
+
+Write-Host "--- Windows fogyasztói élmények letiltása ---" -ForegroundColor Cyan
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
+if (-not (Test-Path $path)) { New-Item $path -Force }
+Set-ItemProperty -Path $path -Name "DisableWindowsConsumerFeatures" -Value 1
+Write-Host "Kész." -ForegroundColor Green
+
+# Alváskezelés visszaállítása alaphelyzetbe
+$type::SetThreadExecutionState([uint32]0x80000000)
+Write-Host "Kész. Az energiagazdálkodási korlátok feloldva." -ForegroundColor Gray
